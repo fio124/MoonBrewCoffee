0b18d6c260dc79eb2c753d4aaefd5eeac51b5c2d
Fio <fisolorzanora@est.utn.ac.cr>
2026-06-03 21:58:01 -0600

    Actualiza productos y agrega orden en procesos

---
+++ Migrations/20260604032954_AgregarOrdenProceso.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MoonBrewCoffee.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarOrdenProceso : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageURL",
+                table: "Productos",
+                newName: "Image64");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Orden",
+                table: "ProcesosPreparacion",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Orden",
+                table: "ProcesosPreparacion");
+
+            migrationBuilder.RenameColumn(
+                name: "Image64",
+                table: "Productos",
+                newName: "ImageURL");
+        }
+    }
+}
